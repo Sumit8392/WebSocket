2d9b6673d444de711ebf1ba77253ff1526379aba
sumitkashyap079 <sumit.kashyap@oodles.io>
2021-10-19 15:39:08 +0530

    WebSocket

---
+++ src/main/java/com/chatroom/ChatBidirection/ChatBidirectionApplication.java
@@ -0,0 +1,13 @@
+package com.chatroom.ChatBidirection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBidirectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBidirectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chatroom/ChatBidirection/ChatBidirectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatroom.ChatBidirection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBidirectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
